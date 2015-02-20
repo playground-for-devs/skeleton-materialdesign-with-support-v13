1459e38390d335b00f6412e20adf0df00fe10b65
Omar Miatello <omar.miatello@satispay.com>
2015-02-20 17:05:43 +0100

    Bluetooth 2.x Scanner

--- app/src/main/java/com/example/pg4devs/playgroundskeleton/MainActivity.java
+++ app/src/main/java/com/example/pg4devs/playgroundskeleton/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.pg4devs.playgroundskeleton;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,10 +21,11 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
     private Toolbar mToolbar;
     private RecyclerView mRecyclerView;
     private SimpleAdapter mAdapter;
-    private static final String[] sCheeseStrings = new String[100];
+    private static final String[] sCheeseStrings = new String[0];
 
     static {
         for (int i = 0; i < sCheeseStrings.length; i++) {
@@ -48,11 +56,11 @@ public class MainActivity extends ActionBarActivity {
                 new ArrayList<SimpleSectionedRecyclerViewAdapter.Section>();
 
         //Sections
-        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(0, "Section 1"));
-        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(5, "Section 2"));
-        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(12, "Section 3"));
-        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(14, "Section 4"));
-        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(20, "Section 5"));
+//        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(0, "Section 1"));
+//        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(5, "Section 2"));
+//        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(12, "Section 3"));
+//        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(14, "Section 4"));
+//        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(20, "Section 5"));
 
         //Add your adapter to the sectionAdapter
         SimpleSectionedRecyclerViewAdapter.Section[] dummy = new SimpleSectionedRecyclerViewAdapter.Section[sections.size()];
@@ -62,8 +70,46 @@ public class MainActivity extends ActionBarActivity {
 
         //Apply this adapter to the RecyclerView
         mRecyclerView.setAdapter(mSectionedAdapter);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+            // Register the BroadcastReceiver
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+            mBluetoothAdapter.startDiscovery();
+//            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+//            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+//            startActivity(discoverableIntent);
+        }
     }
 
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Bluetooth test - ", "Starty");
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                Log.d("Bluetooth test: ", device.getName() + "\n" + device.getAddress());
+                mAdapter.add(String.format("%s (%s)", device.getAddress(), device.getName()), 0);
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
